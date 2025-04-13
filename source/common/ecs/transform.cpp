7383df4bffa52bc4c5fc7fe9151795e0b476beb8
Fatmaz04 <118017527+Fatmaz04@users.noreply.github.com>
2025-04-13 21:30:52 +0200

    Transform

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -10,7 +10,11 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        glm::mat4 T = glm::translate(glm::mat4(1.0f), position);
+        glm::mat4 R = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 S = glm::scale(glm::mat4(1.0f), scale);
+
+        return T * R * S;
     }
 
      // Deserializes the entity data and components from a json object
